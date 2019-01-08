ff9261eebc5658d5fcc68f6fbce4f001e4f898b5
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-08 20:58:47 +0300

    [Chore #163034780]Finish creating 'create_app' function.

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,11 @@ from flask import Flask
 from instance.config import APP_CONFIG
 
 def create_app(config_name):
+    """Create App function."""
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Adding Configurations.
+    app.config.from_object(APP_CONFIG[config_name])
+    app.config.from_pyfile('config.py')
     
+    return app
