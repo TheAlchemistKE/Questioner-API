2df7dae09f33a75a8f0e53aee117000bd644578e
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-08 21:53:10 +0300

    [Start #163035296]Create a BaseCase for testing.

---
+++ app/tests/v1/basecase.py
@@ -0,0 +1,12 @@
+"""Base Case for all tests."""
+import unittest
+import json
+
+# Local Import
+from ... import create_app
+
+
+class TestBaseCase(unittest.TestCase):
+    """Base Testing Class."""
+    def setUp(self):
+        pass
