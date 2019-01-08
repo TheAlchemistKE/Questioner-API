cfc24b15b78ed4eee17d7f616222cff0409abb87
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-08 22:12:04 +0300

    [Chore #163035296]Set up testing environment using setup function.

--- app/tests/v1/basecase.py
+++ app/tests/v1/basecase.py
@@ -9,4 +9,15 @@ from ... import create_app
 class TestBaseCase(unittest.TestCase):
     """Base Testing Class."""
     def setUp(self):
-        pass
+        self.client = create_app(config_name="testing").test_client()
+        self.content_type = "application/json"
+        self.meetup_payload = {
+            "location" : "Nairobi, Kenya",
+            "images" : ["http://www.techsoupeurope.org/wp-content/uploads/2018/07/TheHeorsOfTech-1024x576.png", "https://d1.awsstatic.com/Developer%20Marketing/Events/AWS-Minorities-In-Tech-Site-Banner.77f8c4afbcb5a20525278c95764f1590e4252329.png"],
+            "topic" : "Growing in Tech",
+            "happeningOn" : "31/01/2019",
+            "tags" : ["tech", "growth"]
+        }
+
+    def tearDown(self):
+        self.client
