28393b592a3a51e400c8913329a3705b25e23a10
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-09 11:28:04 +0300

    [Start #163047136]Write tests for getting all questions endpoint.

--- app/tests/v1/test_question.py
+++ app/tests/v1/test_question.py
@@ -16,3 +16,16 @@ class TestQuestion(base):
         response_data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response_data["message"], "Question was created successfully.")
+
+    def test_fetch_all_questions(self):
+        """Test fetching all questions."""
+        post_response = self.client.post('/api/v1/questions', data=json.dumps(self.question_payload), content_type=self.content_type)
+        post_response_data = json.loads(post_response.data.decode())
+        self.assertEqual(post_response.status_code, 201)
+        self.assertEqual(post_response_data["message"], "Question was created successfully.")
+        # Fetching all questions.
+        response = self.client.get('/api/v1/questions', content_type=self.content_type)
+        response_data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data["message"], "Successfully got all questions.")
+
