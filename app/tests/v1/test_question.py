fe484c3a4af194ca1cd881e20a77e1863086236f
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-09 22:10:46 +0300

    [Chore #163037245]Add the post part of the function.

--- app/tests/v1/test_question.py
+++ app/tests/v1/test_question.py
@@ -28,4 +28,12 @@ class TestQuestion(base):
         self.assertEqual(response.status_code, 200)
 
     def test_fetch_single_question(self):
-        """Test fetching a single question"""
+        """Test fetching a single question."""
+        post_response = self.client.post('/api/v1/questions', data=json.dumps(self.question_payload), content_type=self.content_type)
+        post_response_data = json.loads(post_response.data.decode())
+        self.assertEqual(post_response.status_code, 201)
+        self.assertEqual(post_response_data["message"], "Question was created successfully.")
+        # Fetching Single Question.
+        response = self.client.get('api/v1/questions/{}'.format(post_response_data["data"]["id"]), content_type=self.content_type)
+        
+
