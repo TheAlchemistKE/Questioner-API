95578625fa801527b105d54605eab6c0ede4efcc
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-09 16:29:10 +0300

    [Finishes #163086542]Finish writing tests for the fetching all meetups endpoint.

--- app/tests/v1/test_meetup.py
+++ app/tests/v1/test_meetup.py
@@ -4,23 +4,36 @@ import json
 # Local Import
 from .basecase import TestBaseCase as base
 
+
 class TestMeetup(base):
     """Testing the Meetup Endpoints with valid input."""
+
     def setUp(self):
         base.setUp(self)
 
     def test_create_meetup(self):
         """Testing Creation of a Meetup."""
-            
-        response = self.client.post('/api/v1/meetups', data=json.dumps(self.meetup_payload), content_type=self.content_type)
+
+        response = self.client.post(
+            "/api/v1/meetups",
+            data=json.dumps(self.meetup_payload),
+            content_type=self.content_type,
+        )
         response_data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response_data["message"], "Meetup was created successfully.")
 
     def test_fetching_all_meetups(self):
         """Testing Fetching of all meetups."""
-        post_response = self.client.post('/api/v1/meetups', data=json.dumps(self.meetup_payload), content_type=self.content_type)
+        post_response = self.client.post(
+            "/api/v1/meetups",
+            data=json.dumps(self.meetup_payload),
+            content_type=self.content_type
+        )
         post_response_data = json.loads(post_response.data.decode())
         self.assertEqual(post_response.status_code, 201)
-        self.assertEqual(post_response_data["message"], "Meetup was created successfully.")
-
+        self.assertEqual(
+            post_response_data["message"], "Meetup was created successfully."
+        )
+        response = self.client.get("/api/v1/meetups/upcoming", content_type=self.content_type)
+        self.assertEqual(response.status_code, 200)
