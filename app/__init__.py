92d1fc9390866b9a3d93179f90dff49f812e2dfb
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-09 08:05:25 +0300

    [Feature #163036397]Register version blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,15 @@ from instance.config import APP_CONFIG
 def create_app(config_name):
     """Create App function."""
     app = Flask(__name__, instance_relative_config=True)
+    
+    # Registering Blueprint
+    from .api.v1 import version1 as v1
+    app.register_blueprint(v1)
 
     # Adding Configurations.
     app.config.from_object(APP_CONFIG[config_name])
     app.config.from_pyfile('config.py')
+
+
     
     return app
