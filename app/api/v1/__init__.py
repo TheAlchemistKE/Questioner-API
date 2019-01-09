60fd893c144c38c827642bbff6961f01b108d035
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-09 10:54:29 +0300

    [Finishes #163037095]Add questions namespace to v1 init file.

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -6,9 +6,11 @@ from flask import Blueprint
 
 # Local Imports
 from .views.meetup_views import meetup_api
+from .views.question_views import question_api
 
 version1 = Blueprint('Questioner version 1', __name__, url_prefix="/api/v1")
 
 api = Api(version1, version="1.0", title="Questioner REST API", description="This is backend of the Questioner Web app.")
 
 api.add_namespace(meetup_api, path="/meetups")
+api.add_namespace(question_api, path="/questions")
