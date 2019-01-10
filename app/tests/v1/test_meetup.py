d308cd8e2e7990838f0ad8d742bd21d8441262fe
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-11 00:01:34 +0300

    [Chore #163047545]Write tests for create an RSVP endpoint.

--- app/tests/v1/test_meetup.py
+++ app/tests/v1/test_meetup.py
@@ -50,9 +50,8 @@ class TestMeetup(base):
         self.assertEqual(
             post_response_data["message"], "Meetup was created successfully."
         )
-        response = self.client.get("/api/v1/meetup/{}".format(post_response["data"]["id"], content_type=self.content_type))
-        self.assertEqual(response.status_code, 200)
+        
 
     def test_rsvp_to_meetup(self):
         """Test RSVPing to a meetup."""
-        
+
