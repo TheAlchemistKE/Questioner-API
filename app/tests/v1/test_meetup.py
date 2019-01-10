268f94174b6271ade5ce87be351fa2e7b336ef90
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-10 08:12:15 +0300

    [Chore #163101246]Write tests for the get specific meetup endpoint.

--- app/tests/v1/test_meetup.py
+++ app/tests/v1/test_meetup.py
@@ -40,3 +40,14 @@ class TestMeetup(base):
 
     def test_fetch_single_meetup(self):
         """Test fetching a single meetup."""
+        post_response = self.client.post(
+            "/api/v1/meetups",
+            data=json.dumps(self.meetup_payload),
+            content_type=self.content_type
+        )
+        post_response_data = json.loads(post_response.data.decode())
+        self.assertEqual(post_response.status_code, 201)
+        self.assertEqual(
+            post_response_data["message"], "Meetup was created successfully."
+        )
+
