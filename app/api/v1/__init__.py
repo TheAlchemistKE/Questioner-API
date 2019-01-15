32413d8e1486d25b8a3d648d233fe7932372c781
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-15 11:53:20 +0300

    [Chore #163109064]Update the endpoint routes.

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -11,7 +11,8 @@ from .views.rsvp_views import rsvp_api
 
 version1 = Blueprint('Questioner version 1', __name__, url_prefix="/api/v1")
 
-api = Api(version1, version="1.0", title="Questioner REST API", description="This is backend of the Questioner Web app.")
+api = Api(version1, version="1.0", title="Questioner REST API",
+          description="This is backend of the Questioner Web app.", doc="/documentation")
 
 api.add_namespace(meetup_api, path="/meetups")
 api.add_namespace(question_api, path="/questions")
