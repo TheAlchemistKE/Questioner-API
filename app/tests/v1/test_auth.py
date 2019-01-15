3a799581e8bb2a08fad8b7e1425803c517a1c391
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-15 23:35:11 +0300

    [Bug #163226586]Test more authentication scenarios.

--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -1,6 +1,9 @@
 import json
+from ddt import ddt, data
 
 from .basecase import TestBaseCase as base
+
+@ddt
 class TestAuthentication(base):
     """Testing Authentication endpoint"""
     def setUp(self):
@@ -22,7 +25,7 @@ class TestAuthentication(base):
         response = self.client.post('/api/v1/auth/login', data=json.dumps(self.login_payload), content_type=self.content_type)
         response_data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_data[0]["message"], "User login successful.")      
+        self.assertEqual(response_data["message"], "User login successful.")      
 
     def test_logging_in_non_existent_user(self):
         """Testing Logging in a non-existent user."""
@@ -40,6 +43,72 @@ class TestAuthentication(base):
         response_data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 409)
         self.assertEqual(response_data["message"], "Use another email or login.")
+    @data({
+            "firstname": "Kelyn",
+            "lastname": "",
+            "othername": "Paul",
+            "email": "example123@gmail.com",
+            "phoneNumber": "0722997807",
+            "username": "testuser",
+            "password1": "Test@12345",
+            "password2": "Test@12345"
+        },
+        {
+            "firstname": " ",
+            "lastname": "Njeri",
+            "othername": "Paul",
+            "email": "example12378@gmail.com",
+            "phoneNumber": "0722997807",
+            "username": "testuser",
+            "password1": "Test@12345",
+            "password2": "Test@12345"
+        })
+    def testing_entering_wrong_inputs(self, value):
+        response = self.client.post('/api/v1/auth/register', data=json.dumps(value), content_type=self.content_type)
+        response_data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data["message"], "Fields cannot be empty or spaces.")
+    @data(
+        {
+            "firstname": "Kelyn",
+            "lastname": "Njeri",
+            "othername": "Paul",
+            "email": "example12378@gmail.com",
+            "phoneNumber": "0722997807",
+            "username": "testuser",
+            "password1": "Test2345",
+            "password2": "Test@12345"
+        }
+    )
+    def testing_wrong_password(self, value):
+        response = self.client.post('/api/v1/auth/register', data=json.dumps(value), content_type=self.content_type)
+        response_data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data["message"], "Enter correct email. Passwords must match.")
 
-
-        
+    @data({
+            "firstname": "Kelyn",
+            "lastname": "Njeri",
+            "othername": "Paul",
+            "email": "example123@gmailcom",
+            "phoneNumber": "0722997807",
+            "username": "testuser",
+            "password1": "Test@12345",
+            "password2": "Test@12345"
+        },
+        {
+            "firstname": "Kelyn",
+            "lastname": "Njeri",
+            "othername": "Paul",
+            "email": "example12gmail.com",
+            "phoneNumber": "0722997807",
+            "username": "testuser",
+            "password1": "Test@12345",
+            "password2": "Test@12345"
+        })
+    def testing_wrong_email(self, value):
+        response = self.client.post('/api/v1/auth/register', data=json.dumps(value), content_type=self.content_type)
+        response_data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data["message"], "Enter correct email. Passwords must match.")
+    
