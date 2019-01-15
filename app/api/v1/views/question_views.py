0780cc194e5c566f3b97794918fc3f02f4f03d6c
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-15 11:52:47 +0300

    [Chore #163109064]Update question views.

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -3,7 +3,7 @@
 import json
 from flask_restplus import reqparse, Resource
 from flask import Response
-import json
+from werkzeug.exceptions import NotFound
 
 # Local Imports.
 from ..models.question_model import QuestionModel
@@ -19,7 +19,8 @@ parser.add_argument("body", required=True, help="Add Question Body.")
 
 question_request_model = QuestionDataTransferObject.question_request_model
 
-@question_api.route('', '/upcoming')
+
+@question_api.route('')
 class QuestionList(Resource):
     """Question Endpoint."""
     @question_api.expect(question_request_model, validate=True)
@@ -30,66 +31,109 @@ class QuestionList(Resource):
         meetup = request_data["meetup"]
         title = request_data["title"]
         body = request_data["body"]
-
-        new_question = QuestionModel(user, meetup, title, body)
-        save_question = new_question.create_question_record()
+        new_question = dict(
+            user=user,
+            meetup=meetup,
+            title=title,
+            body=body
+        )
+        save_question = QuestionModel.create_question_record(
+            self, data=new_question)
         response_payload = dict(
             status=201,
             message="Question was created successfully.",
             data=save_question
         )
-        response = Response(json.dumps(response_payload), status=201, mimetype="application/json")
+        response = Response(json.dumps(response_payload),
+                            status=201, mimetype="application/json")
         return response
 
     def get(self):
-        """Fetch All Questions.""" 
+        """Fetch All Questions."""
         questions = QuestionModel.fetch_all_questions(self)
         response_payload = {
             "status": 200,
             "data": questions
         }
-        response = Response(json.dumps(response_payload), status=200, mimetype="application/json")
+        response = Response(json.dumps(response_payload),
+                            status=200, mimetype="application/json")
         return response
+
 
 @question_api.route('/<int:question_id>')
 class SingleQuestions(Resource):
     """Deals with all operations on specific questions."""
+
     def get(self, question_id):
-        question = QuestionModel.fetch_specific_question(question_id)
+        question = QuestionModel.fetch_specific_question(self, question_id)
+        if question == "Record doesn't exist.":
+            error_payload = dict(
+                status=404,
+                error="Invalid question id",
+                message="Please enter a valid question id"
+            )
+            error = NotFound()
+            error.data = error_payload
+            raise error
+
         response_payload = {
             "status": 200,
             "data": question
         }
         # Response
-        response = Response(json.dumps(response_payload), status=200, mimetype="application/json")
+        response = Response(json.dumps(response_payload),
+                            status=200, mimetype="application/json")
         return response
+
 
 @question_api.route('/<int:question_id>/upvote')
 class Upvote(Resource):
     """Deals with question upvote."""
+
     def patch(self, question_id):
-        upvote_question = QuestionModel.upvote_question(self, question_id)
-        if upvote_question:
-            response_payload = {
-                "status": 200,
-                "data": upvote_question
-            }
-            response = Response(json.dumps(response_payload), status=200, mimetype="application/json")
-            return response
-        else:
-            return "Error 404."
+        question = QuestionModel.fetch_specific_question(self, question_id)
+        if question == "Record doesn't exist.":
+            error_payload = dict(
+                status=404,
+                error="Invalid question id",
+                message="Please enter a valid question id"
+            )
+            error = NotFound()
+            error.data = error_payload
+            raise error
+
+        upvote_question = QuestionModel.upvote_question(
+            self, question=question)
+        response_payload = {
+            "status": 200,
+            "data": upvote_question
+        }
+        response = Response(json.dumps(response_payload),
+                            status=200, mimetype="application/json")
+        return response
+
 
 @question_api.route('/<int:question_id>/downvote')
 class Downvote(Resource):
     """Deals with question downvote."""
+
     def patch(self, question_id):
-        downvote_question = QuestionModel.downvote_question(self, question_id)
-        if downvote_question:
-            response_payload = {
-                "status": 200,
-                "data": downvote_question
-            }
-            response = Response(json.dumps(response_payload), status=200, mimetype="application/json")
-            return response
-        else:
-            return "Error 404."
+        question = QuestionModel.fetch_specific_question(self, question_id)
+        if question == "Record doesn't exist.":
+            error_payload = dict(
+                status=404,
+                error="Invalid question id",
+                message="Please enter a valid question id"
+            )
+            error = NotFound()
+            error.data = error_payload
+            raise error
+
+        downvote_question = QuestionModel.downvote_question(self, question)
+        response_payload = {
+            "status": 200,
+            "data": downvote_question
+        }
+        response = Response(json.dumps(response_payload),
+                            status=200, mimetype="application/json")
+        return response
